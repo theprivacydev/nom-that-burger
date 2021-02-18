d2237dab91f263feb920172ae086d39f5077ebd9
Pro-Code-art <jayme.coder@gmail.com>
2021-02-17 18:48:58 -0800

    added front-end javascript and ajax calls to assets/js/burger.js

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,1 +1,45 @@
-// document.
+$(function() {
+
+    $('.devour-btn').on('click', (event) => {
+
+        let id = $(this).data('id');
+
+        let burgerStatus = {
+            devoured: true
+        }
+        console.log('right after click burger status is: ', burgerStatus);
+
+        $.ajax('/api/burgers/' + id, {
+            type: "PUT",
+            data: burgerStatus
+        }).then(
+            function() {
+                console.log('burger status is: ', burgerStatus)
+                location.reload();
+            }
+        // End ajax
+        );
+    // End devour-btn event listener
+    });
+
+    $('.make-burger-btn').on('submit', (event) => {
+        event.preventDefault();
+        let madeBurger = {
+            burger_name: $('#input-burger').val().trim(),
+            devoured: 0
+        };
+
+        $.ajax('/api/burgers/', {
+            type: 'POST',
+            data: madeBurger
+        }).then(
+            function() {
+                location.reload();
+            }
+         // End ajax
+        );
+    // End make-burger-btn event listener
+    });
+
+// End containing function
+});
