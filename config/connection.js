645642dcf5ce3fa3242d05b19e55bfc8edf68e77
Pro-Code-art <jayme.coder@gmail.com>
2021-02-18 22:30:56 -0800

    fixed code in connection.js referring to JAWSDB_URL

--- config/connection.js
+++ config/connection.js
@@ -5,14 +5,15 @@ let connection;
 // Defines mysql connection and database we'll be working with
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-    connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: process.env.password,
-    database: 'nom_burgers_db'
-});
+} else {
+        connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: process.env.password,
+        database: 'nom_burgers_db'
+    });
+};
 
 // Makes connection with the mysql database
 connection.connect((err)=> {
